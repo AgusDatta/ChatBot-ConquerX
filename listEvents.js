3cd5d5169c7d0eb9530fc6e486edfa6a10cad90b
Agustin Dattilio <131215517+AgusDatta@users.noreply.github.com>
2024-07-16 03:54:57 -0300

    Update listEvents.js

--- listEvents.js
+++ listEvents.js
@@ -1,4 +1,3 @@
-// listEvents.js
 const { google } = require('googleapis');
 const { format, parseISO, endOfDay, addWeeks } = require('date-fns');
 const { es } = require('date-fns/locale');
@@ -15,23 +14,22 @@ async function listEvents(auth, profileName, sock) {
     const calendar = google.calendar({ version: 'v3', auth });
     const res = await calendar.events.list({
         calendarId: 'primary',
-        timeMin: (new Date()).toISOString(), // Hora actual
-        timeMax: endOfDay(addWeeks(new Date(), 1)).toISOString(), // Final de la semana siguiente
+        timeMin: (new Date()).toISOString(), 
+        timeMax: endOfDay(addWeeks(new Date(), 1)).toISOString(), 
         singleEvents: true,
         orderBy: 'startTime',
     });
     const events = res.data.items;
     if (events.length) {
-        let invalidMeetingsDetails = []; // Inicializar el array para reuniones no válidas
+        let invalidMeetingsDetails = []; 
         let validEvents = await Promise.all(events.map(async event => {
-            // Validar la reunión antes de procesarla
+            
             if (!isValidMeeting(event)) {
                 console.log('Reunión no válida, saltando:', event.summary);
-                invalidMeetingsDetails.push(event.summary); // Agregar a reuniones no válidas
+                invalidMeetingsDetails.push(event.summary); 
                 return null;
             }
 
-            // Procesamiento de la reunión válida
             try {
                 const start = parseISO(event.start.dateTime || event.start.date);
                 const originalDate = new Date(start);
@@ -39,29 +37,23 @@ async function listEvents(auth, profileName, sock) {
                 const summary = event.summary || '';
                 const name = formatName(summary.split(':')[0].trim());
 
-                // Obtener el país y el número limpio basado en la descripción del evento
                 const { country, phoneNumber: cleanedPhoneNumber } = getCountryFromDescription(description);
 
-                // Verificar si el número está en WhatsApp y obtener el JID
                 const { userId, title } = await checkWhatsAppNumber(sock, cleanedPhoneNumber, summary);
                 if (!userId) {
                     console.log(`Número no registrado en WhatsApp: ${cleanedPhoneNumber}`);
                     return null;
                 }
 
-                // Obtener la zona horaria del país
                 const timezone = getTimezoneFromCountry(country);
                 const adjustedTime = moment.tz(start, timezone);
 
-                // Actualizar la hora y día si es necesario
                 let day = adjustedTime.format('D');
                 let weekday = adjustedTime.format('dddd');
                 let time = adjustedTime.format('HH:mm');
 
-                // Logs de información
                 console.log(`Hora original: ${format(originalDate, 'HH:mm', { locale: es })}, País del cliente: ${country}, Hora ajustada: ${time}`);
 
-                // Verificar si hay un cambio de día
                 if (originalDate.getDate() !== adjustedTime.date()) {
                     day = adjustedTime.add(1, 'day').format('D');
                     weekday = adjustedTime.format('dddd');
@@ -79,18 +71,18 @@ async function listEvents(auth, profileName, sock) {
                     phoneNumber: cleanedPhoneNumber,
                     userId,
                     eventId: event.id,
-                    country, // Agregar el país al evento
-                    eventType, // Agregar el tipo de evento
-                    dynamicMessagePart, // Agregar el mensaje dinámico
-                    title // Agregar el título del evento
+                    country,
+                    eventType,
+                    dynamicMessagePart,
+                    title
                 };
             } catch (error) {
                 console.error('Error procesando la reunión:', event.summary, error);
                 return null;
             }
-        })).then(events => events.filter(event => event !== null)); // Filtrar reuniones no válidas
+        })).then(events => events.filter(event => event !== null));
         
-        return { validEvents, invalidMeetingsDetails }; // Devolver eventos válidos y reuniones no válidas
+        return { validEvents, invalidMeetingsDetails };
     } else {
         return { validEvents: [], invalidMeetingsDetails: [] };
     }
